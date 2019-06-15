7b0d2af0d30182bdba7712e90f0fa03f658f4da0
ghigbie <gehigbie@gmail.com>
2019-06-15 17:58:45 -0600

    Add question class

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText;
+  bool quesitonAnswer;
+
+  Question(questionText, questionAnswer){
+    this.questionText = questionText;
+    this.quesitonAnswer = quesitonAnswer;
+  }
+}
