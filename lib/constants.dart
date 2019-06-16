55c2f17f72c34ac27e836df571897f57d9adabff
ghigbie <gehigbie@gmail.com>
2019-06-15 18:07:56 -0600

    Refactor code

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,17 +1,2 @@
 const BUTTON_PADDING = 15.0;
 const FONT_SIZE = 20.0;
-
-// const List <Object> QUESTIONS_LIST = [
-//     {
-//       'question': 'You can lead a cow down stairs but not up stairs.',
-//       'answer': false,
-//     },
-//     {
-//       'question': 'Approximately one quarter of human bones are in the feet.', 
-//       'answer': true,
-//     },
-//     {
-//       'question': 'A slug\'s blood is green.', 
-//       'answer': true,
-//     }
-//   ];
