231e20a76ab89ea3b6cdffbff3e559b51a63cfda
ghigbie <gehigbie@gmail.com>
2019-06-17 20:20:51 -0600

    Modify constructor

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,5 @@ class Question {
   String questionText;
   bool quesitonAnswer;
 
-  Question(String question, bool answer){
-    questionText = question;
-    quesitonAnswer = answer;
-  }
+  Question({String question, bool answer});
 }
